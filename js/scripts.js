181ce3377e69f00662ae924cff05d6ea3c9af571
Dan Kiss and Margaret Berry <dan@>
2017-08-24 16:56:50 -0700

    add board object

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,10 @@
 var whoseTurnNow = "o";
+
+function Board(arrayOneForBoard,arrayTwoForBoard,arrayThreeForBoard){
+  this.arrayOne = arrayOneForBoard;
+  this.arrayTwo = arrayTwoForBoard;
+  this.arrayThree = arrayThreeForBoard;
+}
 var arrayOne = ["","",""];
 var arrayTwo = ["","",""];
 var arrayThree = ["","",""];
@@ -56,41 +62,20 @@ var didIwin = function() {
 //   }
 // }
 
-var player1 = new Player('Player 1', "letter", 1);
-var player2 = new Player('Player 2', "letter", 2);
+// var player1 = new Player('Player 1', "letter", 1);
+// var player2 = new Player('Player 2', "letter", 2);
+var myBoard = new Board(arrayOne, arrayTwo, arrayTwo)
 
 $(document).ready(function() {
-  // var theX = "x";
-  // var theO = "o";
-  // player1.letterName(theX);
-  // player2.letterName(theO);
 
-  // $("#beX").click(function() {
-  //   var theX = "x";
-  //   var theO = "o"
-  //   player1.letterName(theX);
-  //   console.log(player1.letter);
-  //   player2.letterName(theO);
-  //   console.log(player2.letter);
-  // });
-
-  // $("#beO").click(function(){
-  //   var theO = "o"
-  //   var theX = "x";
-  //   console.log(player1.letter);
-  //   console.log(player2.letter);
-  //   player1.letterName(theO);
-  //   console.log(player1.letter);
-  //   player2.letterName(theX);
-  //   console.log(player2.letter);
-  // });
   $('#spot1').click(function(){
     // whoseTurn(whoseTurnNow);
     // console.log(whoseTurnNow);
     $('#spot1').text(whoseTurn(whoseTurnNow));
     // console.log(whoseTurnNow);
-    arrayOne.splice(0, 1, whoseTurnNow);
-    console.log(arrayOne);
+    console.log(myBoard.arrayOne);
+    myBoard.arrayOne.splice(0, 1, whoseTurnNow);
+    console.log(myBoard.arrayOne);
     // next player turn;
     didIwin();
   });
@@ -98,45 +83,45 @@ $(document).ready(function() {
     // console.log(whoseTurnNow);
     $('#spot2').text(whoseTurn(whoseTurnNow));
     // console.log(whoseTurnNow);
-    arrayOne.splice(1, 1, whoseTurnNow);
+    myBoard.arrayOne.splice(1, 1, whoseTurnNow);
     console.log(arrayOne);
     didIwin();
   });
   $('#spot3').click(function(){
     $('#spot3').text(whoseTurn(whoseTurnNow));
-    arrayOne.splice(2, 1, whoseTurnNow);
+    myBoard.arrayOne.splice(2, 1, whoseTurnNow);
     didIwin();
   });
   $('#spot4').click(function(){
     $('#spot4').text(whoseTurn(whoseTurnNow));
-    arrayTwo.splice(0,1,whoseTurnNow);
+    myBoard.arrayTwo.splice(0,1,whoseTurnNow);
     console.log(arrayTwo);
     didIwin();
   });
   $('#spot5').click(function(){
     $('#spot5').text(whoseTurn(whoseTurnNow));
-    arrayTwo.splice(1,1,whoseTurnNow);
+    myBoard.arrayTwo.splice(1,1,whoseTurnNow);
     didIwin();
   });
   $('#spot6').click(function(){
     $('#spot6').text(whoseTurn(whoseTurnNow));
-    arrayTwo.splice(2,1,whoseTurnNow);
+    myBoard.arrayTwo.splice(2,1,whoseTurnNow);
     didIwin();
   });
   $('#spot7').click(function(){
     $('#spot7').text(whoseTurn(whoseTurnNow));
-    arrayThree.splice(0,1,whoseTurnNow);
+    myBoard.arrayThree.splice(0,1,whoseTurnNow);
     console.log(arrayThree);
     didIwin();
   });
   $('#spot8').click(function(){
     $('#spot8').text(whoseTurn(whoseTurnNow));
-    arrayThree.splice(1,1,whoseTurnNow);
+    myBoard.arrayThree.splice(1,1,whoseTurnNow);
     didIwin();
   });
   $('#spot9').click(function(){
     $('#spot9').text(whoseTurn(whoseTurnNow));
-    arrayThree.splice(2,1,whoseTurnNow);
+    myBoard.arrayThree.splice(2,1,whoseTurnNow);
     didIwin();
   });
   $('.playAgain').click(function(event){
